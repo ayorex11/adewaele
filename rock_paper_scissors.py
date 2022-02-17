16dcbd14aec0bd4eff35729280a757e7524d2a77
ayorex11 <97964968+ayorex11@users.noreply.github.com>
2022-02-17 14:54:56 +0100

    Add files via upload

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,39 +1,72 @@
 import random
-rock="r"
-paper="p"
-scissors="s"
-paper>rock
-scissors>paper
-rock>scissors
-trials=4
+
+trials=5
 user_name=input("please enter your name\n>")
 user_name=0
 comp=0
-choices=[rock, paper,scissors]
+choices=["r", "p","s"]
 print("welcome to rock paper scissors!!!")
-while  trials>=0:
+while  trials>0:
     computer=random.choice(choices) 
-    user_input=(input(f"please input your choice {rock} {paper} or {scissors}\n>"))
+    user_input=(input(f"please input your choice r , p or s\n>")).lower()
     if user_input not in choices:
         print("invalid option try again")
+        trials-=1
+        print(f"{trials} trial(s) left")
     if user_input in choices:
-        if user_input>computer:
+        if user_input=="r" and computer =="s" :
+            print(f"computer selected {computer}\n you win!")
             user_name+=1
-            print(computer)
-            print(f"yayy.. you won you have {trials} trial(s) left")
-        if user_input<computer:
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="s" and computer =="r" :
+            print(f"computer selected {computer}\n you lose!")
             comp+=1
-            print(computer)
-            print(f"awwnn.. you lost you have {trials} trial(s) left")
-        if user_input==computer:
-            print(computer)
-            print(f"it's a tie!!!.. play again you have {trials} trial(s) left ")
-    trials-=1  
-print(f"your score is {user_name}")
-print(f"computer's score is {comp}")   
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="p" and computer=="r":
+            print(f"computer selected {computer}\n you win!")
+            user_name+=1
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="r" and computer=="p":
+            print(f"computer selected {computer}\n you lose!")
+            comp+=1
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="p" and computer=="s":
+            print(f"computer selected {computer}\n you lose!")
+            comp+=1
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="s" and computer=="p":
+            print(f"computer selected {computer}\n you win!")
+            user_name+=1
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="p" and computer=="p":
+            print(f"computer selected {computer}\n tie!")
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="s" and computer=="s":
+            print(f"computer selected {computer}\n tie!")
+            trials-=1
+            print(f"{trials} trial(s) left")
+        elif user_input=="r" and computer=="r":
+            print(f"computer selected {computer}\n tie!")
+            trials-=1
+            print(f"{trials} trial(s) left")
+print(user_name)
+print(comp)
 if user_name>comp:
-    print("you win!")
+    print("you win")
 elif comp>user_name:
-    print ("awww.. you lost, better luck next time.")
+    print("you lose better luck next time")
+
+
+
+
+
+        
 
 
